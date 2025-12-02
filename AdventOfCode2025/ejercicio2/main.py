d018954849902a5354009442f4b7f43cbffc0217
masenjo <masenjo@portatilAcer.com>
2025-12-02 16:59:06 +0100

    Añadido cambio en 1 estrella por legibilidad

--- AdventOfCode2025/ejercicio2/main.py
+++ AdventOfCode2025/ejercicio2/main.py
@@ -2,23 +2,22 @@ from math import ceil
 from operator import indexOf
 
 ''' PRIMERA ESTRELLA
-def main() -> None:
-    count = 0
-    with open("input", 'r') as file:
-        for line in file:
-            line = line.split(",")
-            for range_str in line:
-                splitted = range_str.split('-')
-                for num in range(int(splitted[0]), int(splitted[1])):
-                    num_str = str(num)
-                    if num_str[0:ceil(len(num_str)/2)] == num_str[ceil(len(num_str)/2):]:
-                        count+=num
-    print(count)
 '''
+def main() -> None:
+    with open("ejercicio2/input") as archivo:
+        count = 0
+        for line in archivo:
+            for rango in line.split(','):
+                inicio, fin = rango.split('-')
+                for num in range(int(inicio), int(fin) + 1):
+                    num_str = str(num)
+                    if len(num_str) % 2 == 0 and num_str[0:len(num_str) // 2] == num_str[len(num_str) // 2:]:
+                        count += num
+        print(count)
 
 '''
 SEGUNDA ESTRELLA
-'''
+
 def main() -> None:
     count = 0
     with open("input", 'r') as file:
@@ -29,7 +28,7 @@ def main() -> None:
                     if se_repite_mas_de_dos(num):
                         count += num
     print(count)
-
+'''
 def se_repite_mas_de_dos(n) -> bool:
     n_str = str(n)
     n_len = len(n_str)
